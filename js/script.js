2ffa7aa2b618c7d25a36444111f33bf2565a842d
Belyse Uwambayinema <ubelyse1@gmail.com>
2020-10-11 12:05:13 +0200

    added script to handle calculation and prompt

--- js/script.js
+++ js/script.js
@@ -18,8 +18,14 @@ var order = function(number1, number2, number3,number4,number5,number6) {
   });
 
   $(document).ready(function() {
-    $("select.pizzadelivery").change(function () {
-      var val = $('#delivery').val();
-        alert(val);
-    });
+  $('#pizzadelivery').change(function () {
+    var selectedId = $('option:selected', this).attr('id');
+  
+    if (selectedId == "delivery") {
+       var address= prompt("PLease Enter your delivery address");
+       var deliveryfee= $('#delivery').val();
+       alert("Your order will be delivered at: "+ address);
+      alert("The delivery fee is:"+ deliveryfee);
+    }
+  });
 });
